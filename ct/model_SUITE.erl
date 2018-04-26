943d5cbcd405a42d530f1b850acd9843c0cff192
Hugo Mills <hugo@carfax.org.uk>
2018-04-27 00:46:59 +0100

    Fix up tests for lagra_model: Use full URIs for XSD types
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- ct/model_SUITE.erl
+++ ct/model_SUITE.erl
@@ -12,16 +12,22 @@ all() ->
 	 create_literal_05].
 
 create_literal_01(_) ->
-	{literal, {string, "PQR"}} = lagra_model:new_literal("PQR").
+	{literal, {string, "PQR"}}
+		= lagra_model:new_literal("PQR").
 
 create_literal_02(_) ->
-	{literal, {string, "PQR", "en_GB"}} = lagra_model:new_literal("PQR", "en_GB").
+	{literal, {string, "PQR", "en_GB"}}
+		= lagra_model:new_literal("PQR", "en_GB").
 
 create_literal_03(_) ->
-	{literal, {typed, "PQR", "xsd:foo"}} = lagra_model:new_literal_typed("PQR", "xsd:foo").
+	{literal, {typed, "PQR", "http://www.w3.org/2001/XMLSchema#foo"}}
+		= lagra_model:new_literal_typed(
+			"PQR", "http://www.w3.org/2001/XMLSchema#foo").
 
 create_literal_04(_) ->
-	{literal, {typed, 1, "xsd:integer"}} = lagra_model:new_literal(1).
+	{literal, {typed, 1, "http://www.w3.org/2001/XMLSchema#integer"}}
+		= lagra_model:new_literal(1).
 
 create_literal_05(_) ->
-	{literal, {typed, 1.0, "xsd:float"}} = lagra_model:new_literal(1.0).
+	{literal, {typed, 1.0, "http://www.w3.org/2001/XMLSchema#float"}}
+		= lagra_model:new_literal(1.0).
