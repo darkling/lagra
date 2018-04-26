e56a6f394d90d9f7f62fb6654c0308112c6d7182
Hugo Mills <hugo@carfax.org.uk>
2018-04-27 00:48:32 +0100

    Add more function calls to API (without store implementation)
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- src/lagra.erl
+++ src/lagra.erl
@@ -7,8 +7,19 @@
 
 -export([add/2]).
 
--export([po_from_s/2]).
 -export([o_from_sp/3]).
+-export([p_from_so/3]).
+-export([s_from_po/3]).
+-export([po_from_s/2]).
+%-export([so_from_p/2]).
+%-export([sp_from_o/2]).
+
+%-export([o_from_spg/4]).
+%-export([p_from_sog/4]).
+%-export([s_from_pog/4]).
+%-export([po_from_sg/3]).
+%-export([so_from_pg/3]).
+%-export([sp_from_og/3]).
 
 -type store() :: pid().
 
@@ -43,11 +54,24 @@ add(Store, Triple) when tuple_size(Triple) =:= 3 ->
 	gen_server:call(Store, {add_triple, Triple});
 add(Store, Quad) when tuple_size(Quad) =:= 4 ->
 	gen_server:call(Store, {add_quad, Quad}).
--spec po_from_s(store(), lagra_model:subject()) ->
-					   [{lagra_model:predicate(), lagra_model:object()}].
-po_from_s(Store, Subject) ->
-	gen_server:call(Store, {po_from_s, Subject}).
 
-					   [lagra_model:object()].
+-spec o_from_sp(store(), lagra_model:subject(), lagra_model:predicate())
+			   -> [lagra_model:object()].
 o_from_sp(Store, Subject, Predicate) ->
 	gen_server:call(Store, {o_from_sp, Subject, Predicate}).
+
+-spec p_from_so(store(), lagra_model:subject(), lagra_model:object())
+			   -> [lagra_model:predicate()].
+p_from_so(Store, Subject, Object) ->
+	gen_server:call(Store, {p_from_so, Subject, Object}).
+
+-spec s_from_po(store(), lagra_model:predicate(), lagra_model:object())
+			   -> [lagra_model:subject()].
+s_from_po(Store, Predicate, Object) ->
+	gen_server:call(Store, {s_from_po, Predicate, Object}).
+
+
+-spec po_from_s(store(), lagra_model:subject())
+			   -> [{lagra_model:predicate(), lagra_model:object()}].
+po_from_s(Store, Subject) ->
+	gen_server:call(Store, {po_from_s, Subject}).
